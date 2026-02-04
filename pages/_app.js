e29a653ac36d2a654e13e3545638a43e8b9d8a98
saifpatel06 <saifradiocity@gmail.com>
2026-02-04 17:00:05 +0530

    Add Google Analytics integration to MyApp component for improved tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,27 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css';
+import Script from 'next/script'; 
 
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-68EWFF0H8M"
+        strategy="afterInteractive"
+      />
+      
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-68EWFF0H8M');
+        `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
