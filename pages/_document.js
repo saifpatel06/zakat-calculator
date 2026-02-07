cf10f03b6d194fa061742bf9e0c1fc87e6bded2c
saifpatel06 <saifradiocity@gmail.com>
2026-02-07 20:40:47 +0530

    Add favicon image (96x96 PNG) to public directory

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,28 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+
+        {/* Apple icon */}
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+
+        {/* Site name for Google */}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "WebSite",
+              name: "Universal Zakat Calculator",
+              url: "https://zakat-calculator-psi.vercel.app",
+            }),
+          }}
+        />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
